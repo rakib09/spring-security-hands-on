93332751c505aa29f3216403e37269679eaa3521
Rakib Hasan <rakib.hasan@bjitgroup.com>
2020-05-17 00:02:47 +0600

    Initiated project

---
+++ src/main/java/com/extremecoder/springsecurityhandson/SpringSecurityHandsOnApplication.java
@@ -0,0 +1,13 @@
+package com.extremecoder.springsecurityhandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityHandsOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityHandsOnApplication.class, args);
+	}
+
+}
